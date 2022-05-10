bb1d05bf24a96d3574ed61546d50ef3667c422bb
blaze@drakehs <blaze.vincent@drakehs.com>
2022-05-10 14:58:43 -0500

    Successfully test model functionality

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,12 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import connect from "../../db/connect"
+import mongoConnect from "../../db/connect"
+import Test from "../../db/models/test";
 
 export default async function handler(req, res) {
   try {
-    await connect();
-    res.status(200).send('it must be working')
+    await mongoConnect();
+
+    await Test.create({name: 'first', email: 'something2@test.com'})
+
+    const data = await Test.find()
+    res.status(200).json({data});
   } catch (error) {
-    res.status(500).error(error)
+    res.status(500).json({error})
   }
 }
