eec79a347c98398eef9bc9261064924a8336735a
blaze@drakehs <blaze.vincent@drakehs.com>
2022-08-01 13:34:37 -0500

    Remove extraneous database initialization

--- docker-entrypoint-initdb.d/mongo-init.js
+++ docker-entrypoint-initdb.d/mongo-init.js
@@ -1,33 +1,14 @@
 print('Start #################################################################');
 
-db = db.getSiblingDB('api_prod_db');
+db = db.getSiblingDB('api');
 db.createUser(
   {
     user: 'api_user',
     pwd: 'api1234',
-    roles: [{ role: 'readWrite', db: 'api_prod_db' }],
+    roles: [
+      { role: 'readWrite', db: 'api' },
+    ],
   },
 );
-db.createCollection('users');
-
-db = db.getSiblingDB('api_dev_db');
-db.createUser(
-  {
-    user: 'api_user',
-    pwd: 'api1234',
-    roles: [{ role: 'readWrite', db: 'api_dev_db' }],
-  },
-);
-db.createCollection('users');
-
-db = db.getSiblingDB('api_test_db');
-db.createUser(
-  {
-    user: 'api_user',
-    pwd: 'api1234',
-    roles: [{ role: 'readWrite', db: 'api_test_db' }],
-  },
-);
-db.createCollection('users');
 
 print('END #################################################################');
