f941971b4e679f3c14e5f2b7eb1efe6ae778b898
blaze@drakehs <blaze.vincent@drakehs.com>
2022-08-15 16:25:01 -0500

    Add auth testing form

--- pages/dev.js
+++ pages/dev.js
@@ -1,16 +1,29 @@
 import SignupForm from '../components/dev/signupForm';
+import AuthForm from '../components/dev/authForm';
 
+function Section({children}){
+  return <div
+    className='border border-solid border-brand_purple-500 p-4 w-80 rounded'
+  >
+    {children}
+  </div>
+}
 
 export default function Dev(){
-
   return <div
-    className="flex flex-col gap-4"
+    className="flex flex-col gap-8 items-start"
   >
     <h1>developer portal</h1>
 
-    <div>
+    <Section>
       <h2>signup</h2>
       <SignupForm />
-    </div>
+    </Section>
+
+    <Section>
+      <h2>auth</h2>
+      <AuthForm />
+    </Section>
+
   </div>
 }
