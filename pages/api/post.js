b22b482bf6e4bc46abbd529b9c48c16edafaede2
blaze@drakehs <blaze.vincent@drakehs.com>
2022-08-16 11:02:35 -0500

    Handle image file uploading

--- pages/api/post.js
+++ pages/api/post.js
@@ -1,13 +1,51 @@
 import nextConnect from "../../middleware/nextConnect";
 import requireAuthentication from "../../middleware/requireAuthentication";
 import multer from "multer";
+import path from "path";
 
-const upload = multer({dest: __dirname + "/public/uploads"})
+const storage = multer.diskStorage({
+  destination: path.join(__dirname, "../../../../public/uploads"),
+  filename: function(req, file, cb){
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  },
+})
+
+const upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    const mimeWhitelist = ["image/png", "image/jpg", "image/jpeg"]
+    if(mimeWhitelist.includes(file.mimetype)){
+      cb(null, true);
+    } else {
+      if(!req.failedFiles){
+        req.failedFiles = [];
+      }
+      req.failedFiles.push(file.originalname);
+      return cb(null, false);
+    }
+  }
+})
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
 
 const handler = new nextConnect()
 .use(requireAuthentication)
-.post(upload.array('photos'), (req, res) => {
-  console.log(req.body)
+.post(upload.array('image'), (req, res) => {
+  if(req.failedFiles){
+    return res
+      .status(415)
+      .json({
+        error: `The following images were rejected on the basis of file format: ${
+          req.failedFiles.join(', ')
+        }. Accepted file formats are .png, .jpg, and .jpeg.`
+      })
+  }
+
+  //create database objects
   return res.status(200).json({message: "success"})
 })
 
