b806160a5d7b9e942192bdd1f1fbfd229ec37b7c
blaze@drakehs <blaze.vincent@drakehs.com>
2022-08-17 09:53:19 -0500

    Enable the addition of descriptions for individual images

--- components/dev/postForm.js
+++ components/dev/postForm.js
@@ -1,11 +1,13 @@
-import Input from '../form/input'
 import Form from '../form/form';
 import TextArea from '../form/textarea';
 import ImageInput from '../form/imageInput';
+import Input from '../form/input';
+import { useState } from 'react';
 
 export default function PostForm({debug}){
 
   //todo: enable the creation of descriptions for each image
+  const [imageUploads, setImageUploads] = useState([]);
 
   return <Form 
     method='POST'
@@ -19,7 +21,17 @@ export default function PostForm({debug}){
     <ImageInput
       name="image"
       labelText="Images"
-      multiple 
+      multiple
+      onChange={e => {
+        setImageUploads(Array.from(e.target.files))
+      }}
     />
+
+    {imageUploads.map(file => {
+      return <div>
+        <img src={URL.createObjectURL(file)} />
+        <TextArea name={"description-" + file.name} labelText="Image description (optional):" />
+      </div>
+    })}
   </Form>
 }
