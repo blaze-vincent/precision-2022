11f18d3f9e19f6ad4190706c4b6b8e4f133271d7
blaze@drakehs <blaze.vincent@drakehs.com>
2022-08-17 16:39:56 -0500

    Make response messages prettier :)

--- components/form/error.js
+++ components/form/error.js
@@ -1,7 +1,7 @@
 export default function Error({errorCode, errorMessage}){
 
-  return <div className="bg-red-200">
-    <div>Error {errorCode}</div>
-    <div>{errorMessage}</div>
+  return <div className="bg-red-200 p-2 border border-red-300">
+    <b>Error {errorCode}</b>
+    <p>{errorMessage}</p>
   </div>
 }
--- components/form/success.js
+++ components/form/success.js
@@ -1,7 +1,7 @@
 export default function Success({successCode, successMessage}){
 
-  return <div className="bg-green-200">
-    <div>Success {successCode}</div>
-    <div>{successMessage}</div>
+  return <div className="bg-green-200 p-2 border border-green-300">
+    <b>Success {successCode}</b>
+    <p>{successMessage}</p>
   </div>
 }
