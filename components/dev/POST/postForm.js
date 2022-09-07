0d960b4f31f837ea756027c92406ccb87707a14e
blaze@drakehs <blaze.vincent@drakehs.com>
2022-09-07 13:55:38 -0500

    Increase user friendliness of post form

--- components/dev/POST/postForm.js
+++ components/dev/POST/postForm.js
@@ -3,19 +3,26 @@ import TextArea from '../../form/textarea';
 import ImageInput from '../../form/imageInput';
 import { useState } from 'react';
 
-export default function PostForm({debug}){
+function Section({children, offset, className = ''}){
+  return <div
+    className={'w-full h-full absolute overflow-hidden ' + className}
+    style={{
+      left: `${offset*100}%`
+    }}
+  >
+    {children}
+  </div>
+}
 
-  //todo: enable the creation of descriptions for each image
+export default function PostForm(){
+
   const [imageUploads, setImageUploads] = useState([]);
 
-  return <Form 
-    method='POST'
-    apiRoute='post'
-    debug={debug}
-  >
+  const sections = [
     <TextArea 
+      labelText='Describe the job/project:'
       name="textContent"
-    />
+    />,
 
     <ImageInput
       name="image"
@@ -24,13 +31,79 @@ export default function PostForm({debug}){
       onChange={e => {
         setImageUploads(Array.from(e.target.files))
       }}
-    />
+    />,
 
-    {imageUploads.map((file, index) => {
+    ...imageUploads.map((file, index) => {
       return <div key={index}>
         <img src={URL.createObjectURL(file)} />
         <TextArea name={"description-" + file.name} labelText="Image description (optional):" />
       </div>
-    })}
-  </Form>
+    })
+
+  ]
+  
+  const [selectedSection, setSelectedSection] = useState(0);
+  const selectSection = section => {
+    if(section < 0 || section > sections.length){
+      return;
+    }
+    setSelectedSection(section);
+  }
+
+  return <div className='overflow-hidden'>
+    <Form 
+      method='POST'
+      apiRoute='post'
+      className='flex flex-row w-full relative h-80'
+      overrideSubmitButton
+      handleResponse={res => {
+        console.log(res)
+      }}
+    >
+      <div 
+        className='relative w-full h-full transition-transform'
+        style={{
+          transform: `translateX(-${selectedSection * 100}%)`
+        }}
+      >
+
+        {sections.map((section, index) => {
+            return <Section key={index} offset={index}>
+              {section}
+            </Section>
+        })}
+
+        <Section offset={sections.length}
+          className='flex flex-col items-center justify-center'
+        >
+          <button 
+            type="submit" 
+            className='bg-brand_purple-500 p-4 text-white'  
+          >
+            Create Post
+          </button>
+        </Section>
+      </div>
+    </Form>
+
+    <div className='flex flex-row justify-between'>
+      {
+        selectedSection > 0 && <button
+          onClick={_ => {
+            selectSection(selectedSection - 1);
+          }}
+        >back</button> || <div />
+      }
+      {
+        selectedSection < sections.length &&
+        <button
+          onClick={_ => {
+            selectSection(selectedSection + 1);
+          }}
+        >next</button> || <div />
+      }
+    </div>
+
+
+  </div>
 }
