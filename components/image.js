60a490c25625c8983f3b4638669ce13aa9efdfdb
blaze@drakehs <blaze.vincent@drakehs.com>
2022-09-07 13:57:55 -0500

    Improve appearance

--- components/image.js
+++ components/image.js
@@ -1,8 +1,12 @@
 import Image from 'next/image'
 
 export default function Img({src, description}){
-    return <div className='relative w-80 h-40'>
-        <Image src={src} alt='' layout='fill' objectFit='scale-down' />
-        <p>{description}</p>
+  return <div className='w-80 h-40 flex flex-col'>
+    <div className='relative w-full h-full'>
+      <Image src={src} alt={description} layout='fill' objectFit='scale-down' />
     </div>
+    <p
+      className='text-sm font-light'
+    >{description}</p>
+  </div>
 }
