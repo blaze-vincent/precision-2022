fb6408cab659a7421598cc00885c46fd889c84fc
blaze vincent <hikikomareep@pop-os.localdomain>
2022-09-25 12:08:21 -0500

    Abstract compilation of posts; Create util folder

--- pages/posts.js
+++ pages/posts.js
@@ -1,41 +1,11 @@
-import dbConnect from "../db/connect"
-import Post from '../db/models/post';
 import PostEl from '../components/post'
-import Account from '../db/models/account'
-import Image from '../db/models/image'
-import ImagePostAssociation from '../db/models/imagePostAssociation'
-
-import jsonify from "../jsonify";
+import jsonify from "../utils/jsonify";
+import getPosts from '../utils/getPosts';
 
 export async function getServerSideProps(){
-  await dbConnect();
 
-  const posts = await Post.find({}).then(async posts => {
-    const accountCache = {};
-
-    return await Promise.all(posts.map(async post => {
-      const formattedPost = {};
-
-      formattedPost.textContent = post.textContent;
-
-      const acctId = post.f_accountId.toString()
-      if(!accountCache[acctId]){
-        const account = await Account.findOne({_id: post.f_accountId});
-        accountCache[acctId] = account.username
-      }
-      formattedPost.poster = accountCache[acctId];
-
-      //i really wish i would have used a relational database at this point
-      const ipas = await ImagePostAssociation.find({f_postId: post._id});
-      formattedPost.images = await Promise.all(ipas.map(async ipa => {
-        const image = await Image.findOne({_id: ipa.f_imageId});
-        return {url: '/uploads/' + image.resourceURL, description: image.description};
-      }))
-
-      return formattedPost;
-    }))
-  });
-
+  const posts = await getPosts();
+  
   return {
     props: {
       posts: jsonify(posts)
---
+++ utils/getPosts.js
@@ -0,0 +1,37 @@
+import dbConnect from "../db/connect"
+import Post from '../db/models/post';
+import Account from '../db/models/account'
+import Image from '../db/models/image'
+import ImagePostAssociation from '../db/models/imagePostAssociation'
+
+export default async function getPosts(){
+  await dbConnect();
+
+  const posts = await Post.find({}).then(async posts => {
+    const accountCache = {};
+
+    return await Promise.all(posts.map(async post => {
+      const formattedPost = {};
+
+      formattedPost.textContent = post.textContent;
+
+      const acctId = post.f_accountId.toString()
+      if(!accountCache[acctId]){
+        const account = await Account.findOne({_id: post.f_accountId});
+        accountCache[acctId] = account.username
+      }
+      formattedPost.poster = accountCache[acctId];
+
+      //i really wish i would have used a relational database at this point
+      const ipas = await ImagePostAssociation.find({f_postId: post._id});
+      formattedPost.images = await Promise.all(ipas.map(async ipa => {
+        const image = await Image.findOne({_id: ipa.f_imageId});
+        return {url: '/uploads/' + image.resourceURL, description: image.description};
+      }))
+
+      return formattedPost;
+    }))
+  });
+
+  return posts; 
+}
