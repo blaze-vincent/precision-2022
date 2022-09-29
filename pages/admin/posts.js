8ad459cd3fddd2244b78491d4eec0ee1ea75782a
blaze vincent <hikikomareep@pop-os.localdomain>
2022-09-29 17:33:50 -0500

    Copy posts page content; Modify slightly for admin page

--- pages/admin/posts.js
+++ pages/admin/posts.js
@@ -1,5 +1,38 @@
-export default function AdminPosts(){
-  return <div>
-    
+import getPosts from "../../utils/getPosts"
+import WritablePost from "../../components/admin/writablePost";
+import jsonify from "../../utils/jsonify";
+
+export async function getServerSideProps(){
+
+  const posts = await getPosts();
+  console.log(posts)
+  
+  return {
+    props: {
+      posts: jsonify(posts)
+    }
+  }
+}
+
+export default function AdminPosts({posts = []}){
+
+  const postElements = posts.map((post, index) => {
+    return <WritablePost 
+      key={index}
+      images={post.images} 
+      textContent={post.textContent}
+    />
+  })
+
+  return <div
+    className="p-4"
+  >
+  
+    <h1 className="text-xl font-semibold">Posts</h1>
+
+    <div className="flex flex-col gap-4">
+      {postElements.length && postElements || 'Posts could not be loaded.'}
+    </div>
+
   </div>
 }
