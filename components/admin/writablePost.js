8721a0fbc48755846d4781ba04d8b08ada97e1f0
blaze vincent <hikikomareep@pop-os.localdomain>
2022-09-29 17:34:50 -0500

    Add folder for admin page-specific components

---
+++ components/admin/writablePost.js
@@ -0,0 +1,16 @@
+import Img from '../image'
+
+export default function WritablePost({images, textContent}){
+  return <div>
+    <p>
+      {textContent}
+    </p>
+    <div className='flex flex-col gap-2'>
+      {
+        images.map((image, index) => {
+          return <Img key={index} src={image.url} description={image.description} />
+        })
+      }
+    </div>
+  </div>
+}
